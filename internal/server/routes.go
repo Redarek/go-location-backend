59acd114f89ff225a96c21445b3862f8e810d7cb
iiVladIii <vladislav2407@icloud.com>
2024-08-04 23:10:29 +0300

    fix origins for local deploy

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,10 +11,10 @@ import (
 
 func (s *Fiber) RegisterFiberRoutes() {
 	s.App.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
+		//AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
 		AllowOrigins:     config.App.ClientURL,
 		AllowCredentials: true,
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		//AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
 	//s.App.Use(limiter.New(limiter.Config{
