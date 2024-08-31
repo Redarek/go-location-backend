d395a36602f85f503638c92fd6b81b2e37f53eef
Vladislav <vladikdrepa@mail.ru>
2024-08-31 17:15:33 +0300

    Revert "experiment"
    This reverts commit df5440d57643760fa337f6a1521c4c1002634c55.

--- internal/models/user/repository.go
+++
@@ -1,8 +0,0 @@
-package user
-
-type Repository interface {
-	Create(user *User) error
-	GetOne(username string) (*User, error)
-	// GetAll(limit int, offset int) (*[]User, error)
-	// Delete(user *User) (error)
-}
