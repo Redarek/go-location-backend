a6257d9d88bfe07accb16a032fb8c8df20020545
Vladislav <vladikdrepa@mail.ru>
2024-09-26 22:11:17 +0300

    fix: repair AP_type reposotory

---
+++ internal/controller/http/dto/access_point_type.go
@@ -0,0 +1,44 @@
+package dto
+
+import (
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type AccessPointTypeDTO struct {
+	ID        uuid.UUID           `json:"id"`
+	Name      string              `json:"name"`
+	Model     string              `json:"model"`
+	Color     string              `json:"color"`
+	Z         float64             `json:"z"`
+	IsVirtual bool                `json:"isVirtual"`
+	SiteID    uuid.UUID           `json:"site_id"`
+	CreatedAt pgtype.Timestamptz  `json:"createdAt"`
+	UpdatedAt pgtype.Timestamptz  `json:"updatedAt"`
+	DeletedAt *pgtype.Timestamptz `json:"deletedAt"`
+}
+
+type CreateAccessPointTypeDTO struct {
+	Name      string    `json:"name"`
+	Model     string    `json:"model"`
+	Color     string    `json:"color"`
+	Z         float64   `json:"z"`
+	IsVirtual bool      `json:"isVirtual"`
+	SiteID    uuid.UUID `json:"siteId"`
+}
+
+type GetAccessPointTypesDTO struct {
+	SiteID uuid.UUID `json:"id"`
+	Limit  int
+	Offset int
+}
+
+type PatchUpdateAccessPointTypeDTO struct {
+	ID        uuid.UUID `json:"id"`
+	Name      *string   `json:"name"`
+	Model     *string   `json:"model"`
+	Color     *string   `json:"color"`
+	Z         *float64  `json:"z"`
+	IsVirtual *bool     `json:"isVirtual"`
+	// SiteID      *uuid.UUID          `json:"user_id"` // TODO Возможно позже стоит добавить
+}
---
+++ internal/domain/dto/access_point_type.go
@@ -0,0 +1,44 @@
+package dto
+
+import (
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type AccessPointTypeDTO struct {
+	ID        uuid.UUID           `db:"id"`
+	Name      string              `db:"name"`
+	Model     string              `db:"model"`
+	Color     string              `db:"color"`
+	Z         float64             `db:"z"`
+	IsVirtual bool                `db:"is_virtual"`
+	SiteID    uuid.UUID           `db:"site_id"`
+	CreatedAt pgtype.Timestamptz  `db:"created_at"`
+	UpdatedAt pgtype.Timestamptz  `db:"updated_at"`
+	DeletedAt *pgtype.Timestamptz `db:"deleted_at"`
+}
+
+type CreateAccessPointTypeDTO struct {
+	Name      string    `db:"name"`
+	Model     string    `db:"model"`
+	Color     string    `db:"color"`
+	Z         float64   `db:"z"`
+	IsVirtual bool      `db:"is_virtual"`
+	SiteID    uuid.UUID `db:"site_id"`
+}
+
+type GetAccessPointTypesDTO struct {
+	SiteID uuid.UUID `db:"id"`
+	Limit  int
+	Offset int
+}
+
+type PatchUpdateAccessPointTypeDTO struct {
+	ID        uuid.UUID `db:"id"`
+	Name      *string   `db:"name"`
+	Model     *string   `db:"model"`
+	Color     *string   `db:"color"`
+	Z         *float64  `db:"z"`
+	IsVirtual *bool     `db:"is_virtual"`
+	// SiteID      *uuid.UUID          `db:"user_id"` // TODO Возможно позже стоит добавить
+}
